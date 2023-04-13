b354726693800bea8745978e3627c17b4bbe9e04
Slava <kruchkovenko@gmail.com>
2023-04-13 14:28:57 +0500

    create character view

--- app/app/src/main/java/com/example/genshin_wiki/MainActivity.kt
+++ app/app/src/main/java/com/example/genshin_wiki/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.genshin_wiki
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.genshin_wiki.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val count: UInt = 0u;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val bindingActivity: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingActivity.root)
     }
 }
