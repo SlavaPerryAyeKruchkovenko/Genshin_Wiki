c914acd99f01278817947937090504d98ded37bc
Slava <kruchkovenko@gmail.com>
2023-05-09 20:27:26 +0500

    add rectangle images

--- app/app/src/main/java/com/example/genshin_wiki/MainActivity.kt
+++ app/app/src/main/java/com/example/genshin_wiki/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.genshin_wiki
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.genshin_wiki.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     val count: UInt = 0u;
@@ -10,5 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val bindingActivity: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingActivity.root)
+    }
+
+    private fun init(){
+        /*bottomNavigationView.getMenu().findItem(R.id.navigationBarBackground)
+            .setIcon(R.drawable.ic_weapons)
+        bottomNavigationView.setOnNavigationItemSelectedListener(object :
+            OnNavigationItemSelectedListener() {
+            fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
+                when (item.getItemId()) {
+                    R.id.navigation_weapons -> item.getIcon()
+                        .setTint(ContextCompat.getColor(context, R.color.white))
+                }
+                return true
+            }
+        })*/
+
     }
 }
