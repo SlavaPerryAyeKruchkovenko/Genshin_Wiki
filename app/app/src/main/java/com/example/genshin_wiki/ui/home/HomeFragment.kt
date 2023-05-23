46ec3110c519233f213c98b28477188d8de7a4b4
Slava <kruchkovenko@gmail.com>
2023-05-23 17:49:20 +0500

    add header

--- app/app/src/main/java/com/example/genshin_wiki/ui/home/HomeFragment.kt
+++ app/app/src/main/java/com/example/genshin_wiki/ui/home/HomeFragment.kt
@@ -1,23 +1,30 @@
 package com.example.genshin_wiki.ui.home
 
+import android.os.Build
 import android.os.Bundle
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.genshin_wiki.R
+import com.example.genshin_wiki.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        init()
+        return binding.root
+    }
+
+    private fun init() {
+
     }
 }
