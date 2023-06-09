ea059447bafb3ee9068c7eaa07149210c4db1b80
Slava <kruchkovenko@gmail.com>
2023-06-10 02:34:36 +0500

    add login for element of home fragment

--- app/app/src/main/java/com/example/genshin_wiki/ui/home/HomeFragment.kt
+++ app/app/src/main/java/com/example/genshin_wiki/ui/home/HomeFragment.kt
@@ -1,15 +1,20 @@
 package com.example.genshin_wiki.ui.home
 
+import android.content.res.ColorStateList
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.GridLayoutManager
+import com.example.genshin_wiki.R
 import com.example.genshin_wiki.adapters.DungeonAdapter
 import com.example.genshin_wiki.databinding.FragmentHomeBinding
 import com.example.genshin_wiki.models.DungeonResource
+import com.google.android.material.button.MaterialButton
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -35,9 +40,63 @@ class HomeFragment : Fragment() {
             false
         )
         binding.resources.adapter = adapter
-        val observer = Observer<List<DungeonResource>> { newValue ->
+        initDungeonView()
+        initPitchView()
+        initAdapterBtn()
+    }
+
+    private fun initDungeonView() {
+        val dungeonObserver = Observer<List<DungeonResource>> { newValue ->
             adapter.submitList(newValue)
         }
-        viewModel.liveData.observe(viewLifecycleOwner, observer)
+        viewModel.liveData.observe(viewLifecycleOwner, dungeonObserver)
+    }
+
+    private fun initPitchView() {
+        val pitchBtn = binding.reset
+        val pitchObserver = Observer<Int> { newValue ->
+            binding.value.text = newValue.toString()
+        }
+
+        pitchBtn.setOnClickListener {
+            viewModel.resetPitch()
+        }
+        viewModel.pitchData.observe(viewLifecycleOwner, pitchObserver)
+    }
+
+    private fun initAdapterBtn() {
+        val mondstadt = binding.mondstadt
+        val liYue = binding.liYue
+        val inadzuma = binding.inadzuma
+        val sumeru = binding.sumeru
+        val adapterBtns = listOf(mondstadt, liYue, inadzuma, sumeru)
+
+        fun changePosition(btn: MaterialButton) {
+            fun changeColor(materialButton: MaterialButton, attr: Int) {
+                val typedValue = TypedValue()
+                requireContext().theme.resolveAttribute(attr, typedValue, true)
+                val color = typedValue.data
+                materialButton.backgroundTintList = ColorStateList.valueOf(color)
+            }
+            adapterBtns.forEach {
+                changeColor(it, R.attr.colorOnPrimary)
+            }
+            changeColor(btn, R.attr.colorOnPrimarySurface)
+        }
+
+        val positionObserver = Observer<Int> { newValue ->
+            when (newValue) {
+                0 -> changePosition(mondstadt)
+                1 -> changePosition(liYue)
+                2 -> changePosition(inadzuma)
+                3 -> changePosition(sumeru)
+                else -> changePosition(mondstadt)
+            }
+        }
+        mondstadt.setOnClickListener { viewModel.changeAdapterPosition(0) }
+        liYue.setOnClickListener { viewModel.changeAdapterPosition(1) }
+        inadzuma.setOnClickListener { viewModel.changeAdapterPosition(2) }
+        sumeru.setOnClickListener { viewModel.changeAdapterPosition(3) }
+        viewModel.adapterPositionData.observe(viewLifecycleOwner, positionObserver)
     }
 }
--- app/app/src/main/java/com/example/genshin_wiki/ui/home/HomeViewModel.kt
+++ app/app/src/main/java/com/example/genshin_wiki/ui/home/HomeViewModel.kt
@@ -7,9 +7,25 @@ import com.example.genshin_wiki.repository.Mock
 
 class HomeViewModel : ViewModel() {
     val liveData = MutableLiveData<List<DungeonResource>>()
-
+    val pitchData = MutableLiveData<Int>()
+    val adapterPositionData = MutableLiveData<Int>()
     fun init() {
         val resources = Mock().getResources()
         liveData.postValue(resources)
+        pitchData.postValue(153)
+        adapterPositionData.postValue(0)
+    }
+
+    fun resetPitch() {
+        pitchData.postValue(0)
+    }
+
+    fun changeAdapterPosition(position: Int) {
+        if(position in 0..3){
+            adapterPositionData.postValue(position)
+        }
+        else{
+            throw IndexOutOfBoundsException("incorrect index $position of adapter")
+        }
     }
 }
