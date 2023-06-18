a9e1eb92a5df5bbff0bd8c6768240ee10cf759f3
Slava <sh-e-95@mail.ru>
2023-06-18 18:11:24 +0500

    Revert "Merge branch 'splash_screen' into ui"
    This reverts commit 9099bf615140714a01bfe6234b5534252b532389.

--- app/app/src/main/java/com/example/genshin_wiki/models/enums/Elements.kt
+++ app/app/src/main/java/com/example/genshin_wiki/models/enums/Elements.kt
@@ -1,10 +1,5 @@
 package com.example.genshin_wiki.models.enums
 
-import com.example.genshin_wiki.R
-
-enum class Elements(val text: Int,val colorAttr: Int) {
-    PYRO(R.string.pyro,R.attr.colorPyro), HYDRO(R.string.hydro,R.attr.colorHydro),
-    GEO(R.string.geo,R.attr.colorGeo), ELECTRO(R.string.electro,R.attr.colorElectro),
-    DENDRO(R.string.dendro,R.attr.colorDendro), ANEMO(R.string.anemo,R.attr.colorAnemo),
-    CRYO(R.string.cryo,R.attr.colorCryo)
+enum class Elements {
+    PYRO, HYDRO, GEO, ELECTRO, DENDRO, ANEMO, CRYO
 }
