a4137031aefb72385eaaab38288ce28e0dee9abd
Slava <kruchkovenko@gmail.com>
2023-06-20 04:40:41 +0500

    connect liked page with weapons

--- app/app/src/main/java/com/example/genshin_wiki/data/converters/Convert.kt
+++ app/app/src/main/java/com/example/genshin_wiki/data/converters/Convert.kt
@@ -1,6 +1,16 @@
 package com.example.genshin_wiki.data.converters
 
+import com.example.genshin_wiki.data.models.Likeable
+
 sealed class Convert(
-    open val id:String,
+    open val id: String,
     open var isLiked: Boolean
-)
+) {
+    fun toLikeable(): Likeable {
+        return when (this) {
+            is ArtifactConvert -> this.toArtifact()
+            is WeaponConverter -> this.toWeapon()
+            is CharacterConvert -> this.toCharacter()
+        }
+    }
+}
