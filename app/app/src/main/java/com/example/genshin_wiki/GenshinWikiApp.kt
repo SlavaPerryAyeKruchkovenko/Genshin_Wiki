855a889f69b7549fb33b3e0bcbe8679f3b27ca19
Slava <kruchkovenko@gmail.com>
2023-09-17 14:53:52 +0500

    add koin for dependence

---
+++ app/app/src/main/java/com/example/genshin_wiki/GenshinWikiApp.kt
@@ -0,0 +1,18 @@
+package com.example.genshin_wiki
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class GenshinWikiApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // Log Koin into Android logger
+            androidLogger()
+            // Reference Android context
+            androidContext(this@GenshinWikiApp)
+        }
+    }
+}
